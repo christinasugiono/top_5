66018337f4c0e552024ef61256e3ea4914c19e71
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-22 08:47:39 +0800

    feat: validation databrick

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,7 @@
 class Contribution < ApplicationRecord
   belongs_to :board
   belongs_to :user
+  has_many :points
+
+  validates :name, :description, presence: true
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,6 @@
 class Hashtag < ApplicationRecord
+  has_many :board_hashtags
+  has_many :boards, through: :board_hashtags
+
+  validates :title, presence: true
 end
