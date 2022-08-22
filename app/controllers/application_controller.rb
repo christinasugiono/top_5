aee56a62deafbbb7210cd52d46d669be0fdd2cbd
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-22 08:52:22 +0800

    feat: add username to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # For additional fields in app/views/devise/registrations/new.html.erb
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+  # For additional in app/views/devise/registrations/edit.html.erb
+  devise_parameter_sanitizer.permit(:account_update, keys: [:username])
 end
