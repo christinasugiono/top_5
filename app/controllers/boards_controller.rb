2a4caec63fa3921c360bbcf47dc1d45416e807f1
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-25 08:33:06 +0800

    feat: index page hashtag filter

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,12 @@
 class BoardsController < ApplicationController
   def index
-
+    @hashtags = Hashtag.all
+    if params[:hashtag]
+      @query = "##{params[:hashtag]}"
+      @boards = Board.all.select { |board| board.hashtags.include? Hashtag.find_by_title(params[:hashtag]) }
+    else
+      @query = "everything"
+      @boards = Board.all
+    end
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,7 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :board_hashtags
   has_many :hashtags, through: :board_hashtags
+  has_many :contributions
 
   validates :title, :description, presence: true
 end
