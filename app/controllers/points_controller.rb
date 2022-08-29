edc71d30b931cd0a55d2e6fc4ee501df9eec57d4
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-29 13:18:53 +0800

    feat: add contribution to board

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,5 +3,10 @@ class PointsController < ApplicationController
     @contribution = Contribution.find(params[:contribution_id])
     @point = Point.new(contribution: @contribution, user: current_user)
     @point.save
+
+    respond_to do |format|
+      format.html { redirect_to boards_path }
+      format.turbo_stream
+    end
   end
 end
