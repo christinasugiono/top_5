463f8723954f3209aebc486cf4f52e68ec29261e
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-30 10:03:11 +0800

    feat: update order when voted

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -4,6 +4,8 @@ class PointsController < ApplicationController
     @point = Point.new(contribution: @contribution, user: current_user)
     @point.save
 
+    @board = @contribution.board
+    @contributions = @board.contributions.select('contributions.*, COUNT(points.id) AS points_count').left_joins(:points).group('contributions.id').order('points_count DESC').limit(5)
     respond_to do |format|
       format.html { redirect_to boards_path }
       format.turbo_stream
